3015fe247a8a5d4814f37ede578f30bf5a903cbb
khoasdyn <khoasdyn@gmail.com>
2026-01-25 22:19:03 +0700

    Initial Commit

---
+++ flashcard-quiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flashcard-quiz
+//
+//  Created by khoasdyn on 25/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flashcard-quiz/flashcard_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashcard_quizApp.swift
+//  flashcard-quiz
+//
+//  Created by khoasdyn on 25/1/26.
+//
+
+import SwiftUI
+
+@main
+struct flashcard_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
